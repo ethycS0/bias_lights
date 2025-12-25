2c43dc0b59ad33f17e2c99af2d465699782d6869
arjun <arjun.salunkhe@proton.me>
2025-12-25 20:03:29 +0530

    Completed Nix Pkg

--- flake.nix
+++ flake.nix
@@ -17,6 +17,59 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
+        packages.default = pkgs.stdenv.mkDerivation {
+          pname = "blight";
+          version = "0.1.0";
+          src = ./.;
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            gcc
+            makeWrapper
+          ];
+
+          buildInputs = with pkgs; [
+            gst_all_1.gstreamer
+            gst_all_1.gst-plugins-base
+            gst_all_1.gst-plugins-good
+            gst_all_1.gst-plugins-bad
+            pipewire
+            glib
+            libportal
+          ];
+
+          buildPhase = ''
+            gcc -o main src/main.c src/serial.c \
+              $(pkg-config --cflags --libs gstreamer-1.0 gstreamer-app-1.0 libportal glib-2.0)
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp main $out/bin/blight
+
+            mkdir -p $out/share/systemd/user
+            cat > $out/share/systemd/user/blight.service <<EOF
+            [Unit]
+            Description=Bias Lighting for Monitor
+            After=graphical-session.target pipewire.service
+
+            [Service]
+            Type=simple
+            ExecStart=$out/bin/blight 120 1
+            Restart=on-failure
+            RestartSec=5
+
+            [Install]
+            WantedBy=graphical-session.target
+            EOF
+          '';
+
+          postFixup = ''
+            wrapProgram $out/bin/blight \
+              --prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "$GST_PLUGIN_SYSTEM_PATH_1_0"
+          '';
+        };
+
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [
             pkg-config
@@ -29,10 +82,8 @@
             gst_all_1.gst-plugins-base
             gst_all_1.gst-plugins-good
             gst_all_1.gst-plugins-bad
-
             arduino-cli
-                                                python3
-
+            python3
             pipewire
             glib
             libportal
